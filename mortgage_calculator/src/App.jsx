1fa92ec9651739f6c9164f61ac7b8b5e226717e0
vibhanimbalkar97-star <vibhanimbalkar97@gmail.com>
2025-11-07 11:52:24 +0530

    code cleanup
    .

---
+++ mortgage_calculator/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  
+  // question link on dev:https://namastedev.com/practice/mortgage-calculator
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
